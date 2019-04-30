c2a1e1af61f2d3bddde8df2806f007a8f8917d2d
DavidWells <hello@davidwells.io>
2019-04-30 11:23:55 -0700

    verify npm exists before trying to use

--- src/install-deps.js
+++ src/install-deps.js
@@ -4,14 +4,18 @@ const cp = require("child_process");
 const parallelLimit = require("run-parallel-limit");
 const series = require("run-series");
 
-module.exports = function installFunctionDeps(src) {
+module.exports = async function installFunctionDeps(src) {
   const fns = functionGlobber(src)
+
+  const hasNpm = await verifyInstaller('npm')
+  const installCommand = (hasNpm) ? 'npm i' : 'yarn'
+
   return new Promise((resolve, reject) => {
     if (fns.length === 0) return resolve()
 
     const jobs = fns.map(fnPath => {
       return cb => {
-        series([cb => install(fnPath, cb)], cb);
+        series([cb => install(installCommand, fnPath, cb)], cb);
       }
     })
 
@@ -32,6 +36,22 @@ function functionGlobber(baseDir) {
   })
 }
 
-function install(functionDir, cb) {
-  cp.exec("npm i", { cwd: functionDir }, cb);
+function verifyInstaller(type) {
+  if (type !== 'npm' && type !== 'yarn') {
+    throw new Error('Must check npm or yarn')
+  }
+  return new Promise((resolve, reject) => {
+    cp.exec(`which ${type}`, (error, stdout, stderr) => {
+      // swallow errors if "which xyz" fails
+      if (error || stderr || stdout.match((/not found/))) {
+        return resolve(false)
+      }
+      // we have it
+      return resolve(true)
+    })
+  })
+}
+
+async function install(command, functionDir, cb) {
+  cp.exec(command, { cwd: functionDir }, cb);
 }
