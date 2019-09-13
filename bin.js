c072fdda86074c096e801fe7c12d8024a2e65e15
ehmicky <ehmicky@gmail.com>
2019-09-13 12:15:39 +0200

    Fix linting and prettier

--- bin.js
+++ bin.js
@@ -1,60 +1,62 @@
 #!/usr/bin/env node
-const path = require("path");
+const path = require('path')
 
-const minimist = require("minimist");
-const cliOpts = require("cliclopts");
+const minimist = require('minimist')
+const cliOpts = require('cliclopts')
 
-const pkg = require("./package.json");
+const pkg = require('./package.json')
 
-const zipIt = require(".");
+const zipIt = require('.')
 
 const allowedOptions = [
   {
-    name: "zip-go",
-    abbr: "g",
-    help: "zip go binaries",
+    name: 'zip-go',
+    abbr: 'g',
+    help: 'zip go binaries',
     boolean: true,
     default: false
   },
   {
-    name: "help",
-    abbr: "h",
-    help: "show help",
+    name: 'help',
+    abbr: 'h',
+    help: 'show help',
     boolean: true
   },
   {
-    name: "version",
-    abbr: "v",
-    help: "print the version of the program"
+    name: 'version',
+    abbr: 'v',
+    help: 'print the version of the program'
   }
-];
+]
 
-const opts = cliOpts(allowedOptions);
-const argv = minimist(process.argv.slice(2), opts.options());
+const opts = cliOpts(allowedOptions)
+const argv = minimist(process.argv.slice(2), opts.options())
 
-const sourceArg = argv._[0];
-const destArg = argv._[1];
+const sourceArg = argv._[0]
+const destArg = argv._[1]
 
 if (argv.version) {
-  console.log(pkg.version);
-  process.exit();
+  console.log(pkg.version)
+  // eslint-disable-next-line no-process-exit
+  process.exit(0)
 }
 
 if (argv.help || !sourceArg || !destArg) {
-  console.log(
-    `${pkg.name}: Zip lambda functions and their dependencies for deployment\n`
-  );
-  console.log(`Usage: zip-it-and-ship-it [source] [destination] {options}`);
-  opts.print();
-  process.exit(argv.help ? 0 : 1);
+  console.log(`${pkg.name}: Zip lambda functions and their dependencies for deployment\n`)
+  console.log(`Usage: zip-it-and-ship-it [source] [destination] {options}`)
+  opts.print()
+  // eslint-disable-next-line no-process-exit
+  process.exit(argv.help ? 0 : 1)
 }
 
 const source = path.resolve(process.cwd(), sourceArg)
 const dest = path.resolve(process.cwd(), destArg)
 
-zipIt.zipFunctions(source, dest, { skipGo: !argv['zip-go'] })
-.then(console.log)
-.catch(err => {
-  console.error(err.toString())
-  process.exit(1)
-})
+zipIt
+  .zipFunctions(source, dest, { skipGo: !argv['zip-go'] })
+  .then(console.log)
+  .catch(err => {
+    console.error(err.toString())
+    // eslint-disable-next-line no-process-exit
+    process.exit(1)
+  })
