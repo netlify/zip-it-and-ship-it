b1e0030717703624dbb825930d0c9d0840c2e0b0
ehmicky <ehmicky@gmail.com>
2020-05-29 14:17:54 +0200

    Fix typo in variable name

--- src/dependencies.js
+++ src/dependencies.js
@@ -84,7 +84,7 @@ const getModuleDependencies = async function(dependency, basedir, state, package
 const BACKSLASH_REGEXP = /\\/g
 
 const getModuleNameDependencies = async function(moduleName, basedir, state) {
-  if (isExludedModule(moduleName)) {
+  if (isExcludedModule(moduleName)) {
     return []
   }
 
@@ -108,7 +108,7 @@ const getModuleNameDependencies = async function(moduleName, basedir, state) {
   return [...publishedFiles, ...sideFiles, ...depsPaths]
 }
 
-const isExludedModule = function(moduleName) {
+const isExcludedModule = function(moduleName) {
   return EXCLUDED_MODULES.includes(moduleName) || moduleName.startsWith('@types/')
 }
 const EXCLUDED_MODULES = ['aws-sdk']
