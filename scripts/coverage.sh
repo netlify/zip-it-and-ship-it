62caed1bd9fec3012eace1a406ab5747fc8274a8
ehmicky <ehmicky@users.noreply.github.com>
2020-08-10 13:59:11 +0200

    Fix Codecov (#172)

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -7,5 +7,7 @@ os="${os/-latest/}"
 node="$2"
 node="node_${node//./_}"
 
+# We don't use the -Z flag (which makes CI build fail on upload error) because
+# Codecov fails wait too often.
 curl -s https://codecov.io/bash | \
-  bash -s -- -Z -y codecov.yml -f coverage/coverage-final.json -F "$os" -F "$node"
+  bash -s -- -f coverage/coverage-final.json -F "$os" -F "$node"
