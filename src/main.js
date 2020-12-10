69a6d08fb714169d7b2c2eb86dda9dba8bbbe209
David Calavera <david.calavera@gmail.com>
2020-12-09 18:13:30 -0800

    Add metadata file to binary zip files.
    This metadata file, called "netlify-toolchain" allows adding extra information about how the file was generated and what's in it without having to check other files.
    
    Signed-off-by: David Calavera <david.calavera@gmail.com>

--- src/main.js
+++ src/main.js
@@ -52,7 +52,7 @@ const zipFunction = async function(srcPath, destFolder, { skipGo = true, zipGo =
   if (runtime === 'go') {
     if (zipGo) {
       const destPath = join(destFolder, `${filename}.zip`)
-      await zipBinary(srcPath, destPath, filename, stat)
+      await zipBinary(srcPath, destPath, filename, stat, runtime)
       return { path: destPath, runtime }
     } else {
       const destPath = join(destFolder, filename)
@@ -68,7 +68,7 @@ const zipFunction = async function(srcPath, destFolder, { skipGo = true, zipGo =
     // Lambda runtime, and that's the name that AWS
     // expects for those kind of functions.
     const destPath = join(destFolder, `${filename}.zip`)
-    await zipBinary(srcPath, destPath, 'bootstrap', stat)
+    await zipBinary(srcPath, destPath, 'bootstrap', stat, runtime)
     return { path: destPath, runtime }
   }
 }
