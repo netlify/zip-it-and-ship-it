ebd23331f5a4112e66916660035b6ddaa0e11d09
erezrokah <erezrokah@users.noreply.github.com>
2020-12-10 14:33:24 +0100

    refactor: remove redundant try/catch

--- src/fixtures/node-module-next/function.js
+++ src/fixtures/node-module-next/function.js
@@ -1,7 +1,4 @@
-try {
-  // eslint-disable-next-line node/no-extraneous-require
-  require('next/dist/next-server/lib/constants')
-  // eslint-disable-next-line no-empty
-} catch (error) {}
+// eslint-disable-next-line node/no-extraneous-require
+require('next/dist/next-server/lib/constants')
 
 module.exports = true
