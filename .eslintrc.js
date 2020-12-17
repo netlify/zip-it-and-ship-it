65de2f571f3919553d4d227ad482c2086ec43b84
ehmicky <ehmicky@gmail.com>
2020-12-17 16:56:39 +0100

    chore: enable `fp/no-mutating-methods` ESLint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,6 @@ module.exports = {
     'max-lines': 0,
     'max-statements': 0,
     'no-magic-numbers': 0,
-    'fp/no-mutating-methods': 0,
     'import/no-dynamic-require': 0,
     'node/global-require': 0,
   },
