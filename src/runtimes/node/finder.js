74c91518d7edcf92420f908fe57fd28de634db8a
Eduardo Bouças <mail@eduardoboucas.com>
2021-03-12 10:57:25 +0000

    fix: make JS files take precedence over TS files with the same name

--- src/runtimes/node/finder.js
+++ src/runtimes/node/finder.js
@@ -11,14 +11,32 @@ const allowedExtensions = new Set(['.cjs', '.js', '.mjs', '.ts', '.zip'])
 const findFunctionsInPaths = async function (paths) {
   const functions = await Promise.all(paths.map(getFunctionAtPath))
 
-  // We sort the functions array to put directories first. This is so that
-  // `{name}/{name}.js` takes precedence over `{name}.js`.
-  //
   // It's fine to mutate the array since its scope is local to this function.
   // eslint-disable-next-line fp/no-mutating-methods
-  const sortedFunctions = functions
-    .filter(Boolean)
-    .sort((fA, fB) => Number(fA.stat.isDirectory()) - Number(fB.stat.isDirectory()))
+  const sortedFunctions = functions.filter(Boolean).sort((fA, fB) => {
+    // We first sort the functions array to put directories first. This is so
+    // that `{name}/{name}.js` takes precedence over `{name}.js`.
+    const directorySort = Number(fA.stat.isDirectory()) - Number(fB.stat.isDirectory())
+
+    if (directorySort) {
+      return directorySort
+    }
+
+    // We know that we're dealing with two files or two directories, so we sort
+    // them by the full path, in descending order. This conveniently means that
+    // when several files have the same name and different extensions, we place
+    // `.zip` and `.ts` files before `.js` files, which means that JavaScript
+    // files will take precedence once the array is flattened into a Map.
+    if (fA.srcPath < fB.srcPath) {
+      return 1
+    }
+
+    if (fA.srcPath > fB.srcPath) {
+      return -1
+    }
+
+    return 0
+  })
 
   return sortedFunctions
 }
--- tests/main.js
+++ tests/main.js
@@ -402,11 +402,11 @@ testBundlers(
     t.deepEqual(
       functions,
       [
-        { name: 'test', mainFile: 'test.js', runtime: 'js', extension: '.js' },
         { name: 'test', mainFile: 'test.zip', runtime: 'js', extension: '.zip' },
-        { name: 'four', mainFile: 'four.js/four.js.js', runtime: 'js', extension: '.js' },
-        { name: 'one', mainFile: 'one/index.js', runtime: 'js', extension: '.js' },
+        { name: 'test', mainFile: 'test.js', runtime: 'js', extension: '.js' },
         { name: 'two', mainFile: 'two/two.js', runtime: 'js', extension: '.js' },
+        { name: 'one', mainFile: 'one/index.js', runtime: 'js', extension: '.js' },
+        { name: 'four', mainFile: 'four.js/four.js.js', runtime: 'js', extension: '.js' },
         { name: 'test', mainFile: 'test', runtime: 'go', extension: '' },
       ].map(normalizeFiles.bind(null, fixtureDir)),
     )
@@ -422,16 +422,8 @@ testBundlers(
     t.deepEqual(
       functions,
       [
-        { name: 'test', mainFile: 'test.js', runtime: 'js', extension: '.js', srcFile: 'test.js' },
         { name: 'test', mainFile: 'test.zip', runtime: 'js', extension: '.zip', srcFile: 'test.zip' },
-        {
-          name: 'four',
-          mainFile: 'four.js/four.js.js',
-          runtime: 'js',
-          extension: '.js',
-          srcFile: 'four.js/four.js.js',
-        },
-        { name: 'one', mainFile: 'one/index.js', runtime: 'js', extension: '.js', srcFile: 'one/index.js' },
+        { name: 'test', mainFile: 'test.js', runtime: 'js', extension: '.js', srcFile: 'test.js' },
 
         // The JSON file should only be present when using the legacy bundler,
         // since esbuild will inline it within the main file.
@@ -443,6 +435,14 @@ testBundlers(
           srcFile: 'two/three.json',
         },
         { name: 'two', mainFile: 'two/two.js', runtime: 'js', extension: '.js', srcFile: 'two/two.js' },
+        { name: 'one', mainFile: 'one/index.js', runtime: 'js', extension: '.js', srcFile: 'one/index.js' },
+        {
+          name: 'four',
+          mainFile: 'four.js/four.js.js',
+          runtime: 'js',
+          extension: '.js',
+          srcFile: 'four.js/four.js.js',
+        },
         { name: 'test', mainFile: 'test', runtime: 'go', extension: '', srcFile: 'test' },
       ]
         .filter(Boolean)
@@ -604,6 +604,20 @@ testBundlers(
     t.is(require(`${tmpDir}/function.js`).type, 'index-js-file-in-directory')
   },
 )
+
+testBundlers('{name}.js takes precedence over {name}.ts', [ESBUILD, ESBUILD_ZISI, DEFAULT], async (bundler, t) => {
+  const { files, tmpDir } = await zipFixture(t, 'conflicting-names-4', { opts: { jsBundler: bundler } })
+  await unzipFiles(files)
+  // eslint-disable-next-line import/no-dynamic-require, node/global-require
+  t.is(require(`${tmpDir}/function.js`).type, 'function-js-file')
+})
+
+testBundlers('{name}.js takes precedence over {name}.zip', [ESBUILD, ESBUILD_ZISI, DEFAULT], async (bundler, t) => {
+  const { files, tmpDir } = await zipFixture(t, 'conflicting-names-5', { opts: { jsBundler: bundler } })
+  await unzipFiles(files)
+  // eslint-disable-next-line import/no-dynamic-require, node/global-require
+  t.is(require(`${tmpDir}/function.js`).type, 'function-js-file')
+})
 
 testBundlers('Handles a TypeScript function ({name}.ts)', [ESBUILD, ESBUILD_ZISI, DEFAULT], async (bundler, t) => {
   const { files, tmpDir } = await zipFixture(t, 'node-typescript', { opts: { jsBundler: bundler } })
