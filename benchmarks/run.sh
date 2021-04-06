9ff06211885fab6c6e49c49a67f88a93735bcd23
Eduardo Bouças <mail@eduardoboucas.com>
2021-04-06 17:16:19 +0100

    chore: install fixtures deps

--- benchmarks/run.sh
+++ benchmarks/run.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-(cd benchmarks/fixtures/function_1 && npm install);
-(cd benchmarks/fixtures/function_2 && npm install);
+(cd benchmarks/fixtures && npm install);
 node benchmarks/index.js > .delta-t.json
