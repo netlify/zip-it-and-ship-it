a56909ec526f535bee05516a06e5202079c83c13
Eduardo Bouças <mail@eduardoboucas.com>
2021-04-18 07:24:13 +0100

    chore: update benchmarks (#427)
    * chore: update benchmarks
    
    * chore: remove unused input
    
    * chore: pin delta-action to v1

--- benchmarks/esbuild.js
+++ benchmarks/esbuild.js
@@ -3,7 +3,6 @@ const { join } = require('path')
 const { zipFunctions } = require('..')
 
 const { timeFunction } = require('./helpers/main')
-const { runSampleTest } = require('./helpers/sample')
 
 const BENCHMARK_OUTPUT = 'benchmarks/output'
 const RUNS = 3
@@ -11,15 +10,6 @@ const RUNS = 3
 const runBenchmarks = async function () {
   const func = join(__dirname, 'fixtures')
 
-  // eslint-disable-next-line no-magic-numbers
-  const sample = await timeFunction(() => runSampleTest(), RUNS * 1000)
-  const largeDepsZisi = await timeFunction(
-    () =>
-      zipFunctions(func, BENCHMARK_OUTPUT, {
-        config: { '*': { nodeBundler: 'zisi' } },
-      }),
-    RUNS,
-  )
   const largeDepsEsbuild = await timeFunction(
     (run) =>
       zipFunctions(func, join(BENCHMARK_OUTPUT, `run-${run}`), {
@@ -28,9 +18,7 @@ const runBenchmarks = async function () {
     RUNS,
   )
 
-  const output = { metrics: { sample, largeDepsZisi, largeDepsEsbuild } }
-
-  console.log(JSON.stringify(output))
+  console.log(`${largeDepsEsbuild}ms`)
 }
 
 runBenchmarks()
