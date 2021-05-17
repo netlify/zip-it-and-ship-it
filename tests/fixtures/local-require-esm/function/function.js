4778725ef3d32526434ea832be59a5efa67a549b
ehmicky <ehmicky@gmail.com>
2021-05-17 16:13:46 +0200

    chore: fix linting

--- tests/fixtures/local-require-esm/function/function.js
+++ tests/fixtures/local-require-esm/function/function.js
@@ -1,4 +1,4 @@
-// eslint-disable-next-line node/no-unsupported-features/es-syntax
+// eslint-disable-next-line node/no-unsupported-features/es-syntax, import/extensions
 import getZero from './file.js'
 
 // eslint-disable-next-line node/no-unsupported-features/es-syntax
