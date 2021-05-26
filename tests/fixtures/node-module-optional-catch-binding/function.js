aca350ab16f32daefae06c7ab9f579e928412e2d
Eduardo Bouças <mail@eduardoboucas.com>
2021-05-26 15:45:58 +0100

    feat: remove Node 8 support (#478)
    * feat: remove Node 8 support
    
    * chore: fix linting error
    
    * chore: update minimum Node version

--- tests/fixtures/node-module-optional-catch-binding/function.js
+++ tests/fixtures/node-module-optional-catch-binding/function.js
@@ -3,7 +3,6 @@ const noop = () => {}
 module.exports.handler = () => {
   try {
     noop()
-    // eslint-disable-next-line node/no-unsupported-features/es-syntax
   } catch {
     // ¯\_(ツ)_/¯
   }
