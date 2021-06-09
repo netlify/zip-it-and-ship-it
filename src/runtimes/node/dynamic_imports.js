92f64a67160ac9eb0301ae738e38d258117f2b06
Eduardo Bouças <mail@eduardoboucas.com>
2021-06-09 14:38:00 +0100

    feat: replace esbuild with @netlify/esbuild (#516)
    * feat: replace esbuild with @netlify/esbuild
    
    * chore: upgrade @netlify/esbuild
    
    * chore: restore package lock

--- src/runtimes/node/dynamic_imports.js
+++ src/runtimes/node/dynamic_imports.js
@@ -1,44 +1,57 @@
-const { basename, dirname, join } = require('path')
+const { basename, join } = require('path')
 
 const findUp = require('find-up')
 const readPackageJson = require('read-package-json-fast')
 
-const getModulesWithDynamicImports = async ({ srcDir, warnings }) => {
-  const dynamicImportWarnings = warnings
-    // Unfortunately, esbuild doesn't give us any error codes, so this is the
-    // only way we have to single out this case.
-    .filter(({ text }) => text.includes('will not be bundled because the argument is not a string literal'))
-    .map(async ({ location }) => {
+const getDynamicImportsPlugin = ({ moduleNames, srcDir }) => ({
+  name: 'dynamic-imports',
+  setup(build) {
+    const cache = new Map()
+
+    build.onDynamicImport({}, async (args) => {
       try {
-        const packageJsonPath = await findUp(
-          async (directory) => {
-            // We stop traversing if we're about to leave the boundaries of the
-            // function directory or any node_modules directory.
-            if (directory === srcDir || basename(directory) === 'node_modules') {
-              return findUp.stop
-            }
+        const packageName = await getPackageNameCached({ cache, resolveDirectory: args.resolveDir, srcDir })
 
-            const path = join(directory, 'package.json')
-            const hasPackageJson = await findUp.exists(path)
-
-            return hasPackageJson ? path : false
-          },
-          { cwd: dirname(location.file) },
-        )
-
-        if (packageJsonPath !== undefined) {
-          const { name } = await readPackageJson(packageJsonPath)
-
-          return name
+        if (packageName !== undefined) {
+          moduleNames.add(packageName)
         }
       } catch (_) {
-        // We couldn't find a `package.json` or we couldn't get a package name
-        // from it. Either way, it's a no-op.
+        // no-op
       }
     })
-  const packageNames = await Promise.all(dynamicImportWarnings)
+  },
+})
 
-  return [...new Set(packageNames.filter(Boolean))]
+const getPackageName = async ({ resolveDirectory, srcDir }) => {
+  const packageJsonPath = await findUp(
+    async (directory) => {
+      // We stop traversing if we're about to leave the boundaries of the
+      // function directory or any node_modules directory.
+      if (directory === srcDir || basename(directory) === 'node_modules') {
+        return findUp.stop
+      }
+
+      const path = join(directory, 'package.json')
+      const hasPackageJson = await findUp.exists(path)
+
+      return hasPackageJson ? path : false
+    },
+    { cwd: resolveDirectory },
+  )
+
+  if (packageJsonPath !== undefined) {
+    const { name } = await readPackageJson(packageJsonPath)
+
+    return name
+  }
 }
 
-module.exports = { getModulesWithDynamicImports }
+const getPackageNameCached = ({ cache, resolveDirectory, srcDir }) => {
+  if (!cache.has(resolveDirectory)) {
+    cache.set(resolveDirectory, getPackageName({ resolveDirectory, srcDir }))
+  }
+
+  return cache.get(resolveDirectory)
+}
+
+module.exports = { getDynamicImportsPlugin }
