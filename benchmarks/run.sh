ff270672bd0f65b979bc6a8dd839d2dd7df5fd89
Charlie <charlie.groll@netlify.com>
2021-09-14 09:56:51 -0400

    fix: accidentally checked in test code

--- benchmarks/run.sh
+++ benchmarks/run.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
-exec 3>>.delta.largeDepsZisi
-exec 4>>.delta.largeDepsEsbuild
-exec 5>>.delta.largeDepsZisi.withFlag
-exec 6>>.delta.largeDepsEsbuild.withFlag
-
 npm ci --prefix benchmarks/fixtures
-node benchmarks/zisi.js >&3
-node benchmarks/esbuild.js >&4
-NETLIFY_EXPERIMENTAL_DEFAULT_ES_MODULES_TO_ES_BUILD=true node benchmarks/zisi.js >&5
-NETLIFY_EXPERIMENTAL_DEFAULT_ES_MODULES_TO_ES_BUILD=true node benchmarks/esbuild.js >&6
+node benchmarks/zisi.js > .delta.largeDepsZisi
+node benchmarks/esbuild.js > .delta.largeDepsEsbuild
