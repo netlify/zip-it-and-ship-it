f788e9640c46b5dfba7055382f8e120d18c5051f
Netlify Team Account 1 <netlify-team-account-1@users.noreply.github.com>
2021-09-24 16:41:52 +0200

    chore: ignore fixtures for eslint

--- tests/fixtures/invalid-require/function.js
+++ tests/fixtures/invalid-require/function.js
@@ -1,6 +1,4 @@
-// eslint-disable-next-line no-constant-condition
 if (false) {
-  // eslint-disable-next-line import/no-unresolved, import/no-unassigned-import, node/global-require
   require('@scope')
 }
 
