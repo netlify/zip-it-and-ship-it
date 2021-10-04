f436fa8090b28cb65f58718451b01893114a5b8b
Eduardo Bouças <mail@eduardoboucas.com>
2021-10-04 18:11:24 +0100

    chore: run some tests in serial

--- tests/helpers/test_many.js
+++ tests/helpers/test_many.js
@@ -1,3 +1,5 @@
+const { platform } = require('process')
+
 const makeTestMany = (test, matrix) => {
   const testBundlers = (title, variationNames, assertions, testFn = test) => {
     variationNames.forEach((name) => {
@@ -9,7 +11,12 @@ const makeTestMany = (test, matrix) => {
 
       const testTitle = `${title} [${name}]`
 
-      testFn(testTitle, assertions.bind(null, variation))
+      // Weird workaround to avoid running too many tests in parallel on
+      // Windows, which causes problems in the CI.
+      const isSerial = variationNames.length >= 3 && platform === 'win32'
+      const testFunction = isSerial ? testFn.serial : testFn
+
+      testFunction(testTitle, assertions.bind(null, variation))
     })
   }
 
