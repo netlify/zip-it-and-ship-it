ae502b534c2fc7910926f23320edf21a6497b29f
Eduardo Bouças <mail@eduardoboucas.com>
2021-10-04 17:03:32 +0100

    refactor: convert export type

--- src/node_dependencies/tree_files.ts
+++ src/node_dependencies/tree_files.ts
@@ -6,16 +6,16 @@ import glob from 'glob'
 const pGlob = promisify(glob)
 
 // When using a directory, we include all its descendants except `node_modules`
-const getTreeFiles = function (srcPath: string, stat: Stats) {
+const getTreeFiles = async function (srcPath: string, stat: Stats): Promise<string[]> {
   if (!stat.isDirectory()) {
     return [srcPath]
   }
 
-  return pGlob(`${srcPath}/**`, {
+  return await pGlob(`${srcPath}/**`, {
     ignore: `${srcPath}/**/node_modules/**`,
     nodir: true,
     absolute: true,
   })
 }
 
-module.exports = { getTreeFiles }
+export { getTreeFiles }
