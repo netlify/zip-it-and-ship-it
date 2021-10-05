3ef4abe7fa6e91e9f1657b4e18187ade118971f5
Netlify Team Account 1 <netlify-team-account-1@users.noreply.github.com>
2021-10-05 12:48:45 +0200

    chore: move examples into type-level tests

--- tests/helpers/globify.ts
+++ tests/helpers/globify.ts
@@ -1,10 +1,12 @@
 import { AssertTrue, IsExact } from 'conditional-type-checks'
 
 // takes a string like `foo_bar`, recursively splits it up along underscores, and collects all kinds of glob expressions.
-// for `foo_bar`, it'd evaluate to `'*' | 'foo*' | 'foo_*' | 'foo_bar'`.
+// see examples below.
 export type Globify<S extends string> = S extends `${infer Head}_${infer Tail}`
   ? '*' | `${Head}*` | `${Head}_${Globify<Tail>}`
   : S | '*'
+
+type TestFoobar = AssertTrue<IsExact<Globify<'foo_bar'>, '*' | 'foo*' | 'foo_*' | 'foo_bar'>>
 
 type TestBundlers = AssertTrue<
   IsExact<
