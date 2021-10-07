27842d6cf7b66e5d31724a18dd0e1faafef6ea64
Netlify Team Account 1 <netlify-team-account-1@users.noreply.github.com>
2021-10-07 17:09:27 +0200

    chore: package is a reserved keyword

--- src/runtimes/node/bundlers/esbuild/plugin_native_modules.js
+++ src/runtimes/node/bundlers/esbuild/plugin_native_modules.js
@@ -29,15 +29,15 @@ const getNativeModulesPlugin = (externalizedModules) => ({
 
     // eslint-disable-next-line complexity, max-statements
     build.onResolve({ filter: packageFilter }, async (args) => {
-      const package = packageName.exec(args.path)
+      const pack = packageName.exec(args.path)
 
-      if (!package) return
+      if (!pack) return
 
       let directory = args.resolveDir
 
       while (true) {
         if (path.basename(directory) !== 'node_modules') {
-          const modulePath = path.join(directory, 'node_modules', package[1])
+          const modulePath = path.join(directory, 'node_modules', pack[1])
           const packageJsonPath = path.join(modulePath, 'package.json')
           // eslint-disable-next-line no-await-in-loop
           const [isNative, packageJsonData] = await findNativeModule(packageJsonPath, cache)
