3e76706497d3866ee1639c12e390d8b7898b094f
Netlify Team Account 1 <netlify-team-account-1@users.noreply.github.com>
2021-10-11 10:02:04 +0200

    chore: ignore unused type for next line

--- tests/helpers/globify.ts
+++ tests/helpers/globify.ts
@@ -6,8 +6,10 @@ export type Globify<S extends string> = S extends `${infer Head}_${infer Tail}`
   ? '*' | `${Head}*` | `${Head}_${Globify<Tail>}`
   : S | '*'
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 type TestFoobar = AssertTrue<IsExact<Globify<'foo_bar'>, '*' | 'foo*' | 'foo_*' | 'foo_bar'>>
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 type TestBundlers = AssertTrue<
   IsExact<
     Globify<'bundler_default' | 'bundler_esbuild_zisi'>,
