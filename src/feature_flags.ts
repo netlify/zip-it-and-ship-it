462f7f8313656d0ea6cffeb6749bf94e022f1e0b
Eduardo Bouças <mail@eduardoboucas.com>
2021-10-15 15:18:20 +0100

    refactor: migrate feature_flags to TypeScript

--- src/feature_flags.ts
+++ src/feature_flags.ts
@@ -1,14 +1,16 @@
-const { env } = require('process')
+import { env } from 'process'
+
+type FeatureFlag = 'buildGoSource' | 'buildRustSource' | 'defaultEsModulesToEsbuild' | 'parseWithEsbuild'
 
 // List of supported flags and their default value.
-const FLAGS = {
+const FLAGS: Record<FeatureFlag, boolean> = {
   buildGoSource: Boolean(env.NETLIFY_EXPERIMENTAL_BUILD_GO_SOURCE),
   buildRustSource: Boolean(env.NETLIFY_EXPERIMENTAL_BUILD_RUST_SOURCE),
   defaultEsModulesToEsbuild: Boolean(env.NETLIFY_EXPERIMENTAL_DEFAULT_ES_MODULES_TO_ESBUILD),
   parseWithEsbuild: false,
 }
 
-const getFlags = (input = {}, flags = FLAGS) =>
+const getFlags = (input: Record<string, boolean> = {}, flags = FLAGS) =>
   Object.entries(flags).reduce(
     (result, [key, defaultValue]) => ({
       ...result,
@@ -17,4 +19,5 @@ const getFlags = (input = {}, flags = FLAGS) =>
     {},
   )
 
-module.exports = { FLAGS, getFlags }
+export { getFlags }
+export type { FeatureFlag }
