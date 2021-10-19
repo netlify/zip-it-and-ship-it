10d83e0e74b07bd20e004740875ad8f6fb2e906d
Eduardo Bouças <mail@eduardoboucas.com>
2021-10-19 09:57:49 +0100

    refactor: remove duplication of FeatureFlags keys

--- src/feature_flags.ts
+++ src/feature_flags.ts
@@ -1,15 +1,16 @@
 import { env } from 'process'
 
-type FeatureFlag = 'buildGoSource' | 'buildRustSource' | 'defaultEsModulesToEsbuild' | 'parseWithEsbuild'
-type FeatureFlags = Record<FeatureFlag, boolean>
-
-// List of supported flags and their default value.
-const FLAGS: Record<FeatureFlag, boolean> = {
+const FLAGS: Record<string, boolean> = {
   buildGoSource: Boolean(env.NETLIFY_EXPERIMENTAL_BUILD_GO_SOURCE),
   buildRustSource: Boolean(env.NETLIFY_EXPERIMENTAL_BUILD_RUST_SOURCE),
   defaultEsModulesToEsbuild: Boolean(env.NETLIFY_EXPERIMENTAL_DEFAULT_ES_MODULES_TO_ESBUILD),
   parseWithEsbuild: false,
 }
+
+type FeatureFlag = keyof typeof FLAGS
+type FeatureFlags = Record<FeatureFlag, boolean>
+
+// List of supported flags and their default value.
 
 const getFlags = (input: Record<string, boolean> = {}, flags = FLAGS) =>
   Object.entries(flags).reduce(
