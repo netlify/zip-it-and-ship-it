c3d01f8ee18e655229949d973d99fe2eeeb08a6b
Eduardo Bouças <mail@eduardoboucas.com>
2021-10-19 18:43:51 +0100

    refactor: migrate main file

---
+++ src/main.ts
@@ -0,0 +1,92 @@
+import { extname } from 'path'
+
+import './utils/polyfills'
+import { Config } from './config'
+import { FeatureFlags, getFlags } from './feature_flags'
+import { FunctionSource } from './function'
+import { getFunctionsFromPaths } from './runtimes'
+import { getPluginsModulesPath } from './runtimes/node/utils/plugin_modules_path'
+import { GetSrcFilesFunction, RuntimeName } from './runtimes/runtime'
+import { listFunctionsDirectories, resolveFunctionsDirectories } from './utils/fs'
+import { zipFunction, zipFunctions } from './zip'
+
+interface ListedFunction {
+  name: string
+  mainFile: string
+  runtime: RuntimeName
+  extension: string
+}
+
+type ListedFunctionFile = ListedFunction & {
+  srcFile: string
+}
+
+interface ListFunctionsOptions {
+  basePath?: string
+  config?: Config
+  featureFlags?: FeatureFlags
+  pluginsModulesPath?: string
+}
+
+// List all Netlify Functions main entry files for a specific directory
+const listFunctions = async function (
+  relativeSrcFolders: string | string[],
+  { featureFlags: inputFeatureFlags }: { featureFlags?: FeatureFlags } = {},
+) {
+  const featureFlags = getFlags(inputFeatureFlags)
+  const srcFolders = resolveFunctionsDirectories(relativeSrcFolders)
+  const paths = await listFunctionsDirectories(srcFolders)
+  const functions = await getFunctionsFromPaths(paths, { featureFlags })
+  const listedFunctions = [...functions.values()].map(getListedFunction)
+  return listedFunctions
+}
+
+// List all Netlify Functions files for a specific directory
+const listFunctionsFiles = async function (
+  relativeSrcFolders: string | string[],
+  { basePath, config, featureFlags: inputFeatureFlags }: ListFunctionsOptions = {},
+) {
+  const featureFlags = getFlags(inputFeatureFlags)
+  const srcFolders = resolveFunctionsDirectories(relativeSrcFolders)
+  const paths = await listFunctionsDirectories(srcFolders)
+  const [functions, pluginsModulesPath] = await Promise.all([
+    getFunctionsFromPaths(paths, { config, featureFlags }),
+    getPluginsModulesPath(srcFolders[0]),
+  ])
+  const listedFunctionsFiles = await Promise.all(
+    [...functions.values()].map((func) => getListedFunctionFiles(func, { basePath, featureFlags, pluginsModulesPath })),
+  )
+
+  return listedFunctionsFiles.flat()
+}
+
+const getListedFunction = function ({ runtime, name, mainFile, extension }: FunctionSource): ListedFunction {
+  return { name, mainFile, runtime: runtime.name, extension }
+}
+
+const getListedFunctionFiles = async function (
+  func: FunctionSource,
+  options: { basePath?: string; featureFlags: FeatureFlags; pluginsModulesPath?: string },
+): Promise<ListedFunctionFile[]> {
+  const srcFiles = await getSrcFiles({ ...func, ...options })
+  const { name, mainFile, runtime } = func
+
+  return srcFiles.map((srcFile) => ({ srcFile, name, mainFile, runtime: runtime.name, extension: extname(srcFile) }))
+}
+
+const getSrcFiles: GetSrcFilesFunction = async function ({ extension, runtime, srcPath, ...args }) {
+  const { getSrcFiles: getRuntimeSrcFiles } = runtime
+
+  if (extension === '.zip' || typeof getRuntimeSrcFiles !== 'function') {
+    return [srcPath]
+  }
+
+  return await getRuntimeSrcFiles({
+    extension,
+    runtime,
+    srcPath,
+    ...args,
+  })
+}
+
+export { zipFunctions, zipFunction, listFunctions, listFunctionsFiles }
