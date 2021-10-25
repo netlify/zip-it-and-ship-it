72aee8d09a65537c55ef1423cd8688cb5433c663
Eduardo Bouças <mail@eduardoboucas.com>
2021-10-25 11:53:57 +0100

    feat: add Manifest type

--- src/manifest.ts
+++ src/manifest.ts
@@ -4,11 +4,28 @@ import { arch, platform } from 'process'
 import { FunctionResult } from './utils/format_result'
 import { writeFile } from './utils/fs'
 
+interface ManifestFunction {
+  mainFile: string
+  name: string
+  path: string
+  runtime: string
+}
+
+interface Manifest {
+  functions: ManifestFunction[]
+  system: {
+    arch: string
+    platform: string
+  }
+  timestamp: number
+  version: number
+}
+
 const MANIFEST_VERSION = 1
 
 const createManifest = async ({ functions, path }: { functions: FunctionResult[]; path: string }) => {
   const formattedFunctions = functions.map(formatFunctionForManifest)
-  const payload = {
+  const payload: Manifest = {
     functions: formattedFunctions,
     system: { arch, platform },
     timestamp: Date.now(),
@@ -26,3 +43,4 @@ const formatFunctionForManifest = ({ mainFile, name, path, runtime }: FunctionRe
 })
 
 export { createManifest }
+export type { Manifest }
