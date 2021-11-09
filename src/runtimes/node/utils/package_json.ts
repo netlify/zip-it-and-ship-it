6c82adada6507fd9f2fc31abf742d29e7c69b6a6
Netlify Team Account 1 <90322326+netlify-team-account-1@users.noreply.github.com>
2021-11-09 15:51:58 +0100

    fix: sanitise package.json files field (#800)
    * fix: sanitise package.json files field
    
    * Apply suggestions from code review
    
    Co-authored-by: Eduardo Bouças <mail@eduardoboucas.com>
    
    Co-authored-by: Netlify Team Account 1 <netlify-team-account-1@users.noreply.github.com>
    Co-authored-by: Eduardo Bouças <mail@eduardoboucas.com>

--- src/runtimes/node/utils/package_json.ts
+++ src/runtimes/node/utils/package_json.ts
@@ -14,6 +14,19 @@ interface PackageJson {
   type?: string
 }
 
+const sanitiseFiles = (files: unknown): string[] | undefined => {
+  if (!Array.isArray(files)) {
+    return undefined
+  }
+
+  return files.filter((file) => typeof file === 'string')
+}
+
+const sanitisePackageJson = (packageJson: Record<string, unknown>): PackageJson => ({
+  ...packageJson,
+  files: sanitiseFiles(packageJson.files),
+})
+
 // Retrieve the `package.json` of a specific project or module
 const getPackageJson = async function (srcDir: string): Promise<PackageJson> {
   const packageRoot = await pkgDir(srcDir)
@@ -25,11 +38,12 @@ const getPackageJson = async function (srcDir: string): Promise<PackageJson> {
   const packageJsonPath = `${packageRoot}/package.json`
   try {
     // The path depends on the user's build, i.e. must be dynamic
-    // eslint-disable-next-line import/no-dynamic-require, node/global-require
-    return require(packageJsonPath)
+    // eslint-disable-next-line import/no-dynamic-require, node/global-require, @typescript-eslint/no-var-requires
+    const packageJson = require(packageJsonPath)
+    return sanitisePackageJson(packageJson)
   } catch (error) {
     throw new Error(`${packageJsonPath} is invalid JSON: ${error.message}`)
   }
 }
 
-export { getPackageJson, PackageJson }
+export { getPackageJson, PackageJson, sanitisePackageJson }
