826aa4d43624984ba8881ada119c1d9f8d3d2f5a
ehmicky <ehmicky@users.noreply.github.com>
2022-02-08 13:16:32 +0100

    chore: use common `FIXTURES_DIR` in benchmarks (#975)

--- benchmarks/esbuild.js
+++ benchmarks/esbuild.js
@@ -2,17 +2,15 @@ const { join } = require('path')
 
 const { zipFunctions } = require('..')
 
-const { timeFunction } = require('./helpers/main')
+const { timeFunction, FIXTURES_DIR } = require('./helpers/main')
 
 const BENCHMARK_OUTPUT = 'benchmarks/output'
 const RUNS = 3
 
 const runBenchmarks = async function () {
-  const func = join(__dirname, 'fixtures')
-
   const largeDepsEsbuild = await timeFunction(
     (run) =>
-      zipFunctions(func, join(BENCHMARK_OUTPUT, `run-${run}`), {
+      zipFunctions(FIXTURES_DIR, join(BENCHMARK_OUTPUT, `run-${run}`), {
         config: { '*': { nodeBundler: 'esbuild' } },
       }),
     RUNS,
--- benchmarks/nft.js
+++ benchmarks/nft.js
@@ -1,18 +1,15 @@
-const { join } = require('path')
 
 const { zipFunctions } = require('../dist/main')
 
-const { timeFunction } = require('./helpers/main')
+const { timeFunction, FIXTURES_DIR } = require('./helpers/main')
 
 const BENCHMARK_OUTPUT = 'benchmarks/output'
 const RUNS = 3
 
 const runBenchmarks = async function () {
-  const func = join(__dirname, 'fixtures')
-
   const largeDepsNft = await timeFunction(
     () =>
-      zipFunctions(func, BENCHMARK_OUTPUT, {
+      zipFunctions(FIXTURES_DIR, BENCHMARK_OUTPUT, {
         config: { '*': { nodeBundler: 'nft' } },
       }),
     RUNS,
