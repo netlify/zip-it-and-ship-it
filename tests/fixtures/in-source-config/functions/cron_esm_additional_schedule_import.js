cff11ed6be5a78e7d25e70813fde4cd696010507
Jenae Janzen <101715009+jenae-janzen@users.noreply.github.com>
2022-06-27 15:35:00 -0400

    chore: add test

--- tests/fixtures/in-source-config/functions/cron_esm_additional_schedule_import.js
+++ tests/fixtures/in-source-config/functions/cron_esm_additional_schedule_import.js
@@ -2,7 +2,7 @@ import { schedule as nfySchedule } from '@netlify/functions'
 import { schedule } from '../node_modules/@netlify/functions/index.js'
 // make sure cron expression is found/doesn't error if `schedule` is also imported from another source
 
-schedule();
+schedule()
 
 export const handler = nfySchedule('@daily', () => {
   // function handler
