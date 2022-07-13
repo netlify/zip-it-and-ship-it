c051c492108fc9f59a1748da52caee4461d22fd4
Antonio Jesús <antonio.rodriguez@netlify.com>
2022-07-13 11:59:37 +0200

    chore: fixed failing unit test

--- tests/unit/runtimes/node/utils/node_version.js
+++ tests/unit/runtimes/node/utils/node_version.js
@@ -7,11 +7,11 @@ const {
 } = require('../../../../../dist/runtimes/node/utils/node_version.js')
 
 test('getNodeVersion', (t) => {
+  t.is(getNodeVersion('nodejs14.x'), 14)
   t.is(getNodeVersion('nodejs12.x'), 12)
   t.is(getNodeVersion('nodejs8.x'), 8)
   t.is(getNodeVersion('12.x'), 12)
   t.is(getNodeVersion('8.x'), 8)
-  t.is(getNodeVersion('node14'), 14)
   t.is(getNodeVersion('node16'), DEFAULT_NODE_VERSION)
   t.is(getNodeVersion(':shrug:'), DEFAULT_NODE_VERSION)
 })
