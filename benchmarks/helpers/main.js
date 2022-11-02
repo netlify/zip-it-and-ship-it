45877636768dfbdb1ad5c2a5aa54ed6dc2e4fbb8
Daniel Tschinder <231804+danez@users.noreply.github.com>
2022-11-02 10:00:29 +0100

    chore: fix benchmark (#1243)

--- benchmarks/helpers/main.js
+++ benchmarks/helpers/main.js
@@ -1,10 +1,20 @@
+/* eslint-disable max-statements */
+import assert from 'assert'
 import { performance, PerformanceObserver } from 'perf_hooks'
 import { fileURLToPath } from 'url'
 
 export const FIXTURES_DIR = fileURLToPath(new URL('../fixtures', import.meta.url))
 
+const sleep = (ms) =>
+  new Promise((resolve) => {
+    setTimeout(resolve, ms)
+  })
+
 export const timeFunction = async (func, runs = 1) => {
   const finishedRuns = new Map()
+
+  performance.clearMarks()
+  performance.clearMeasures()
 
   const observer = new PerformanceObserver((list) => {
     const [entry] = list.getEntries()
@@ -14,17 +24,23 @@ export const timeFunction = async (func, runs = 1) => {
 
   observer.observe({ entryTypes: ['measure'] })
 
-  await Promise.all(
-    Array.from({ length: runs }).map(async (_, index) => {
-      performance.mark(`run-${index}-start`)
+  for (let index = 0; index < runs; index++) {
+    performance.mark(`run-${index}-start`)
 
-      await func(index)
+    await func(index)
 
-      performance.measure(`run-${index}`, `run-${index}-start`)
-    }),
-  )
+    performance.measure(`run-${index}`, `run-${index}-start`)
+  }
+
+  // wait for PerformanceObserver to gather all data
+  await sleep(100)
 
   const durations = [...finishedRuns.values()]
+
+  observer.disconnect()
+
+  assert(durations.length === runs, 'Not all runs produced timings')
+
   const average = durations.reduce((acc, duration) => duration + acc, 0) / runs
 
   return average
