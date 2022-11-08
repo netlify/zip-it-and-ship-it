d315ee22a0006cecd11e34c5e4e267bd5b1f8201
Daniel Tschinder <231804+danez@users.noreply.github.com>
2022-11-08 15:48:43 +0100

    chore: remove default import workaround

--- src/runtimes/node/bundlers/nft/index.ts
+++ src/runtimes/node/bundlers/nft/index.ts
@@ -1,7 +1,7 @@
 import { basename, dirname, join, normalize, resolve } from 'path'
 
 import { nodeFileTrace } from '@vercel/nft'
-import nftResolveDependency from '@vercel/nft/out/resolve-dependency.js'
+import resolveDependency from '@vercel/nft/out/resolve-dependency.js'
 
 import type { FunctionConfig } from '../../../../config.js'
 import { FeatureFlags } from '../../../../feature_flags.js'
@@ -13,11 +13,6 @@ import { filterExcludedPaths, getPathsOfIncludedFiles } from '../../utils/includ
 import type { GetSrcFilesFunction, BundleFunction } from '../types.js'
 
 import { processESM } from './es_modules.js'
-
-// @ts-expect-error vitest with esbuild already resolves the default export, whereas Node.js does not
-const resolveDependency: typeof nftResolveDependency.default = nftResolveDependency.default
-  ? nftResolveDependency.default
-  : nftResolveDependency
 
 // Paths that will be excluded from the tracing process.
 const ignore = ['node_modules/aws-sdk/**']
@@ -118,7 +113,7 @@ const traceFilesAndTranspile = async function ({
     },
     resolve: async (specifier, parent, ...args) => {
       try {
-        return await resolveDependency(specifier, parent, ...args)
+        return await resolveDependency.default(specifier, parent, ...args)
       } catch (error) {
         // If we get a `MODULE_NOT_FOUND` error for what appears to be a module
         // name, we try to resolve it a second time using `pluginsModulesPath`
@@ -126,7 +121,7 @@ const traceFilesAndTranspile = async function ({
         if (error.code === 'MODULE_NOT_FOUND' && pluginsModulesPath && appearsToBeModuleName(specifier)) {
           const newParent = join(pluginsModulesPath, basename(parent))
 
-          return await resolveDependency(specifier, newParent, ...args)
+          return await resolveDependency.default(specifier, newParent, ...args)
         }
 
         throw error
