67c39ea7aace77960af14ec825c88970e61d128e
khen <30577427+khendrikse@users.noreply.github.com>
2023-01-10 13:21:19 +0100

    test: add unit test for checkIsInternalFunction

--- src/utils/check_is_internal_function.ts
+++ src/utils/check_is_internal_function.ts
@@ -1,8 +1,8 @@
 import { FUNCTIONS_INTERNAL_DIR } from '../runtimes/constants.js'
 
-export const checkIsInternalFunction = (srcDir: string) => {
+export const checkIsInternalFunction = (srcDir = '') => {
   const BACKSLASH_REGEXP = /\\/g
 
   // Backslashes need to be converted for Windows.
-  return srcDir.replace(BACKSLASH_REGEXP, '/').includes(FUNCTIONS_INTERNAL_DIR)
+  return srcDir?.replace(BACKSLASH_REGEXP, '/').includes(FUNCTIONS_INTERNAL_DIR)
 }
