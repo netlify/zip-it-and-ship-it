9fbc9877b7aa700527bf70a79a921bb38b881709
Daniel Tschinder <231804+danez@users.noreply.github.com>
2023-04-04 11:56:11 +0200

    chore: increase test timeout

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   test: {
     setupFiles: ['./tests/helpers/vitest_setup.ts'],
     include: ['tests/**/*.test.ts'],
-    testTimeout: 30_000,
+    testTimeout: 60_000,
     deps: {
       external: ['**/fixtures/**', '**/node_modules/**', '**/dist/**'],
       interopDefault: false,
