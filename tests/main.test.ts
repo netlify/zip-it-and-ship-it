6ee1776ed513bf0ce847ae0464a31f8e7aed78b8
Eduardo Bouças <mail@eduardoboucas.com>
2023-04-21 12:07:34 +0100

    chore: conditionally run test

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -2700,21 +2700,23 @@ describe('zip-it-and-ship-it', () => {
     },
   )
 
-  testMany(
-    'Handles a JavaScript function using the V2 API',
-    ['bundler_default', 'todo:bundler_esbuild', 'todo:bundler_esbuild_zisi', 'bundler_default_nft', 'bundler_nft'],
-    async (options) => {
-      const { files, tmpDir } = await zipFixture('v2-api', {
-        opts: merge(options, { featureFlags: { zisi_functions_api_v2: true } }),
-      })
-      await unzipFiles(files)
+  if (semver.gte(nodeVersion, '18.13.0')) {
+    testMany(
+      'Handles a JavaScript function using the V2 API',
+      ['bundler_default', 'todo:bundler_esbuild', 'todo:bundler_esbuild_zisi', 'bundler_default_nft', 'bundler_nft'],
+      async (options) => {
+        const { files, tmpDir } = await zipFixture('v2-api', {
+          opts: merge(options, { featureFlags: { zisi_functions_api_v2: true } }),
+        })
+        await unzipFiles(files)
 
-      const func = await importFunctionFile(`${tmpDir}/function.js`)
-      const { body, headers = {}, statusCode } = await invokeLambda(func)
+        const func = await importFunctionFile(`${tmpDir}/function.js`)
+        const { body, headers = {}, statusCode } = await invokeLambda(func)
 
-      expect(body).toBe('<h1>Hello world</h1>')
-      expect(headers['content-type']).toBe('text/html')
-      expect(statusCode).toBe(200)
-    },
-  )
+        expect(body).toBe('<h1>Hello world</h1>')
+        expect(headers['content-type']).toBe('text/html')
+        expect(statusCode).toBe(200)
+      },
+    )
+  }
 })
