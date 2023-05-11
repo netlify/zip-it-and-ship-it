73bdcb2882ed5a49c9708cb27fa18a7df5557337
Daniel Tschinder <231804+danez@users.noreply.github.com>
2023-05-11 23:40:22 +0200

    chore: revert hook timeout

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,7 +6,6 @@ export default defineConfig({
     setupFiles: ['./tests/helpers/vitest_setup.ts'],
     include: ['tests/**/*.test.ts'],
     testTimeout: 60_000,
-    hookTimeout: 120_000,
     deps: {
       external: ['**/fixtures/**', '**/node_modules/**', '**/dist/**'],
       interopDefault: false,
