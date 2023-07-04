25b10ebb83cc2fa934ebdc1d83b34c1cfdc3b254
Simon Knott <info@simonknott.de>
2023-07-04 17:46:57 +0200

    fix: auto-enable zisi_pure_esm, zisi_pure_esm_mjs when v2 api is used (#1489)
    * fix: auto-enable zisi_pure_esm, zisi_pure_esm_mjs when v2 api is used
    
    * refactor: read runtimeVersion besides flags

--- src/runtimes/node/index.ts
+++ src/runtimes/node/index.ts
@@ -62,6 +62,7 @@ const zipFunction: ZipFunction = async function ({
 
   const inSourceConfig = await findISCDeclarationsInPath(mainFile, name, featureFlags)
   const runtimeAPIVersion = inSourceConfig.runtimeAPIVersion === 2 ? 2 : 1
+
   const pluginsModulesPath = await getPluginsModulesPath(srcDir)
   const bundlerName = await getBundlerName({
     config,
--- tests/v2api.test.ts
+++ tests/v2api.test.ts
@@ -27,7 +27,28 @@ describe.runIf(semver.gte(nodeVersion, '18.13.0'))('V2 functions API', () => {
     async (options) => {
       const { files } = await zipFixture('v2-api', {
         opts: merge(options, {
-          featureFlags: { zisi_functions_api_v2: true, zisi_pure_esm: true, zisi_pure_esm_mjs: true },
+          featureFlags: { zisi_functions_api_v2: true },
+        }),
+      })
+      const unzippedFunctions = await unzipFiles(files)
+
+      const func = await importFunctionFile(`${unzippedFunctions[0].unzipPath}/${files[0].entryFilename}`)
+      const { body: bodyStream, headers = {}, statusCode } = await invokeLambda(func)
+      const body = await readAsBuffer(bodyStream)
+
+      expect(body).toBe('<h1>Hello world</h1>')
+      expect(headers['content-type']).toBe('text/html')
+      expect(statusCode).toBe(200)
+    },
+  )
+
+  testMany(
+    'Handles a .mjs function',
+    ['bundler_default', 'bundler_esbuild', 'bundler_esbuild_zisi', 'bundler_default_nft', 'bundler_nft'],
+    async (options) => {
+      const { files } = await zipFixture('v2-api-mjs', {
+        opts: merge(options, {
+          featureFlags: { zisi_functions_api_v2: true },
         }),
       })
       const unzippedFunctions = await unzipFiles(files)
@@ -49,7 +70,7 @@ describe.runIf(semver.gte(nodeVersion, '18.13.0'))('V2 functions API', () => {
       const { files, tmpDir } = await zipFixture('v2-api', {
         opts: merge(options, {
           archiveFormat: ARCHIVE_FORMAT.NONE,
-          featureFlags: { zisi_functions_api_v2: true, zisi_pure_esm: true, zisi_pure_esm_mjs: true },
+          featureFlags: { zisi_functions_api_v2: true },
         }),
       })
 
@@ -71,7 +92,7 @@ describe.runIf(semver.gte(nodeVersion, '18.13.0'))('V2 functions API', () => {
       const { files, tmpDir } = await zipFixture('v2-api-ts', {
         opts: merge(options, {
           archiveFormat: ARCHIVE_FORMAT.NONE,
-          featureFlags: { zisi_pure_esm: true, zisi_functions_api_v2: true },
+          featureFlags: { zisi_functions_api_v2: true },
         }),
       })
 
