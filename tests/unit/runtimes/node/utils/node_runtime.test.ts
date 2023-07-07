eb6ba611dad0cd2218e9cc237004b71f0a91ed8c
Daniel Tschinder <231804+danez@users.noreply.github.com>
2023-07-07 12:17:36 +0200

    fix: select at least Node.js 18 when v2 function detected (#1490)

--- tests/unit/runtimes/node/utils/node_runtime.test.ts
+++ tests/unit/runtimes/node/utils/node_runtime.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, test } from 'vitest'
 
-import { getNodeRuntime } from '../../../../../src/runtimes/node/utils/node_runtime.js'
+import { getNodeRuntime, getNodeRuntimeForV2 } from '../../../../../src/runtimes/node/utils/node_runtime.js'
 
 describe('getNodeRuntime', () => {
   test.each([
@@ -15,5 +15,25 @@ describe('getNodeRuntime', () => {
     [':shrug:', undefined],
   ])('handles `%s`', (input, expected) => {
     expect(getNodeRuntime(input)).toBe(expected)
+  })
+})
+
+describe('getNodeRuntimeForV2', () => {
+  test.each([
+    ['nodejs12.x', 'nodejs18.x'],
+    ['nodejs16.x', 'nodejs18.x'],
+    ['nodejs18.x', 'nodejs18.x'],
+    // enable once supported
+    // ['nodejs20.x', 'nodejs20.x'],
+    ['14.x', 'nodejs18.x'],
+    ['v16.x', 'nodejs18.x'],
+    ['18.0.0', 'nodejs18.x'],
+    // ['20.0.0', 'nodejs20.x'],
+    ['v14.2.0', 'nodejs18.x'],
+    ['14.1', 'nodejs18.x'],
+    [':shrug:', 'nodejs18.x'],
+    ['99.0.0', undefined],
+  ])('handles `%s`', (input, expected) => {
+    expect(getNodeRuntimeForV2(input)).toBe(expected)
   })
 })
