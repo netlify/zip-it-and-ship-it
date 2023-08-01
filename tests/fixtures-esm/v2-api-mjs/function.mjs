922070ca51eaf55f295cb06c65e57cf1422b7d15
Eduardo Bouças <mail@eduardoboucas.com>
2023-08-01 16:59:31 +0100

    chore: formatting

--- tests/fixtures-esm/v2-api-mjs/function.mjs
+++ tests/fixtures-esm/v2-api-mjs/function.mjs
@@ -1,5 +1,6 @@
-export default async () => new Response("<h1>Hello world</h1>", {
-  headers: {
-    "content-type": "text/html"
-  }
-})
+export default async () =>
+  new Response('<h1>Hello world</h1>', {
+    headers: {
+      'content-type': 'text/html',
+    },
+  })
