241b49f9a32e38364df06c41ea52348fe2bb25cf
Eduardo Bouças <mail@eduardoboucas.com>
2023-08-02 14:03:39 +0100

    fix: account for CJS and ESM entry files

--- src/runtimes/node/utils/entry_file.ts
+++ src/runtimes/node/utils/entry_file.ts
@@ -33,7 +33,8 @@ const getEntryFileContents = (
     return [
       `import * as func from '${importPath}'`,
       `import * as bootstrap from './${BOOTSTRAP_FILE_NAME}'`,
-      `export const handler = bootstrap.getLambdaHandler(func)`,
+      'const funcModule = typeof func.default === "function" ? func : func.default',
+      `export const handler = bootstrap.getLambdaHandler(funcModule)`,
     ].join(';')
   }
 
