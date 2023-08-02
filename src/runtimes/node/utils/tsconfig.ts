864994671e36110049bc3d5034660ae9679ef3f3
Eduardo Bouças <mail@eduardoboucas.com>
2023-08-02 12:43:15 +0100

    chore: add comment

--- src/runtimes/node/utils/tsconfig.ts
+++ src/runtimes/node/utils/tsconfig.ts
@@ -4,6 +4,8 @@ import { MODULE_FORMAT } from './module_format.js'
 
 const esmModuleValues = new Set(['es2020', 'es2022', 'esnext', 'node16', 'nodenext'])
 
+// Returns the module format that should be used for a TypeScript file at a
+// given path, by reading the associated `tsconfig.json` file if it exists.
 export const getModuleFormat = (path: string) => {
   const file = getTsconfig(path)
 
