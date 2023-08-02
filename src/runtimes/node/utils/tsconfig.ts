217633cb74db61f145105a820d16a91405eb3692
Eduardo Bouças <mail@eduardoboucas.com>
2023-08-02 16:36:11 +0100

    fix: emit ESM for `es6` and `es2015`

--- src/runtimes/node/utils/tsconfig.ts
+++ src/runtimes/node/utils/tsconfig.ts
@@ -2,7 +2,7 @@ import { getTsconfig } from 'get-tsconfig'
 
 import { MODULE_FORMAT } from './module_format.js'
 
-const esmModuleValues = new Set(['es2020', 'es2022', 'esnext', 'node16', 'nodenext'])
+const esmModuleValues = new Set(['es6', 'es2015', 'es2020', 'es2022', 'esnext', 'node16', 'nodenext'])
 
 // Returns the module format that should be used for a TypeScript file at a
 // given path, by reading the associated `tsconfig.json` file if it exists.
