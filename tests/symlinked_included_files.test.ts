db2f95984d77640e31e8006978822e134e91700e
Simon Knott <info@simonknott.de>
2024-02-14 14:41:55 +0100

    fix: create symlink parent directory before creating symlink itself (#1709)
    * fix: don't swallow beginning of path
    
    * chore: add repro
    
    * fix: create link parent directory before creating link

--- tests/symlinked_included_files.test.ts
+++ tests/symlinked_included_files.test.ts
@@ -17,7 +17,7 @@ const readDirWithType = async (dir: string, readFiles?: Record<string, boolean>,
 
   for (const dirent of dirents) {
     if (dirent.isDirectory()) {
-      await readDirWithType(join(dir, dirent.name), files, dirent.name)
+      await readDirWithType(join(dir, dirent.name), files, join(parent, dirent.name))
     } else {
       files[join(parent, dirent.name)] = dirent.isSymbolicLink()
     }
@@ -34,7 +34,7 @@ test.skipIf(platform() === 'win32')('Symlinked directories from `includedFiles` 
   // assert on the source files
   expect(await readDirWithType(basePath)).toEqual({
     'function.mjs': false,
-    [join('crazy-dep/package.json')]: false,
+    [join('node_modules/.pnpm/crazy-dep/package.json')]: false,
     [join('node_modules/crazy-dep')]: true,
   })
 
@@ -63,7 +63,42 @@ test.skipIf(platform() === 'win32')('Symlinked directories from `includedFiles` 
     '___netlify-bootstrap.mjs': false,
     '___netlify-entry-point.mjs': false,
     'function.mjs': false,
-    [join('crazy-dep/package.json')]: false,
+    [join('node_modules/.pnpm/crazy-dep/package.json')]: false,
     [join('node_modules/crazy-dep')]: true,
+  })
+})
+
+test.skipIf(platform() === 'win32')('symlinks in subdir of `includedFiles` are copied over successfully', async () => {
+  const { path: tmpDir } = await getTmpDir({ prefix: 'zip-it-test' })
+  const basePath = join(FIXTURES_ESM_DIR, 'symlinked-bin')
+  const mainFile = join(basePath, 'function.ts')
+
+  // assert on the source files
+  expect(await readDirWithType(basePath)).toEqual({
+    'function.ts': false,
+    [join('subproject/node_modules/.bin/cli.js')]: true,
+    [join('subproject/node_modules/tool/cli.js')]: false,
+  })
+
+  await zipFunction(mainFile, tmpDir, {
+    archiveFormat: ARCHIVE_FORMAT.NONE,
+    basePath,
+    config: {
+      '*': {
+        includedFiles: ['subproject/**'],
+      },
+    },
+    repositoryRoot: basePath,
+    systemLog: console.log,
+    debug: true,
+    internalSrcFolder: undefined,
+  })
+
+  expect(await readDirWithType(join(tmpDir, 'function'))).toEqual({
+    '___netlify-bootstrap.mjs': false,
+    '___netlify-entry-point.mjs': false,
+    'function.cjs': false,
+    'subproject/node_modules/.bin/cli.js': true,
+    'subproject/node_modules/tool/cli.js': false,
   })
 })
