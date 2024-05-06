347e99f3ace63eb59ca47c591a196b9b1f6bc3f7
Simon Knott <info@simonknott.de>
2024-05-06 13:25:06 +0200

    chore: prettier

--- tests/v2api.test.ts
+++ tests/v2api.test.ts
@@ -401,8 +401,8 @@ describe.runIf(semver.gte(nodeVersion, '18.13.0'))('V2 functions API', () => {
     for (const file of files) {
       switch (file.name) {
         case 'with-literal':
-          expect(file.generator).toBe("foo@1.2.3")
-          expect(file.displayName).toBe("Product List")
+          expect(file.generator).toBe('foo@1.2.3')
+          expect(file.displayName).toBe('Product List')
           expect(file.routes).toEqual([{ pattern: '/products', literal: '/products', methods: ['GET', 'POST'] }])
 
           break
